bef47843ba5b290a07f3e66d6ff1cf7f092f46a7
elvin <elvindanielmolo@ufps.edu.co>
2025-05-27 12:06:27 -0500

    dsi

--- src/app/enviroment/apiurl.ts
+++ src/app/enviroment/apiurl.ts
@@ -1,7 +1,7 @@
 export const apiurl = {
 
-    //apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://localhost:8080'
 
-    apiUrl: 'https://ventas-backend-production-ee8b.up.railway.app'
+    //apiUrl: 'https://ventas-backend-production-ee8b.up.railway.app'
 
   };
